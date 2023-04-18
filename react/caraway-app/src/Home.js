1866b9ff449dae43d2710ffffaf217a68feaeeb5
Safiya Mustafa <87836647+safiyamustafa@users.noreply.github.com>
2023-04-18 10:47:54 -0500

    added image

--- react/caraway-app/src/Home.js
+++ react/caraway-app/src/Home.js
@@ -4,7 +4,11 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="project-title"><a className="title-home" >caraway</a></h1>
+      {/* <h1 className="project-title"><a className="title-home" >caraway</a></h1> */}
+      <img 
+          src={require('./homeCaraway.jpg')} 
+          alt="HomeImage" 
+        />
     </div>
   );
 }
