de8027e17b7cfe11838872527950ff0ffab44bf2
Safiya Mustafa <87836647+safiyamustafa@users.noreply.github.com>
2023-04-19 17:30:08 -0500

    added links to tips headers

--- react/caraway-app/src/Management.js
+++ react/caraway-app/src/Management.js
@@ -9,23 +9,39 @@ export default function Home() {
         <div className="management-grid">
         
           <div className="management-tip">
-            <h1 className="management-tip-header">Tips On Balancing School, Work and Life</h1>
+            <h1 className="management-tip-header"> 
+              <a href="https://jhsap.org/self_help_resources/school-life_balance/">
+              Tips On Balancing School, Work and Life
+              </a>
+            </h1>
             <p className="management-tip-content">Having a hard time with school, work and trying to have a social life? Look at these tips to help balance them.</p>
 
           </div>
 
           <div className="management-tip">
-            <h1 className="management-tip-header">10 Ways to Manage Feeling Anxious</h1>
+            <h1 className="management-tip-header">
+              <a href="https://cognitivebehaviortherapycenter.com/10-ways-to-manage-anxiety-worry-stress/">
+                10 Ways to Manage Anxiety
+              </a>              
+            </h1>
             <p className="management-tip-content">Feeling anxious and don’t know how to manage anxiety from workload? Try out one of these 10 tips!</p>
           </div>
 
           <div className="management-tip">
-            <h1 className="management-tip-header">Preparing for Busy Weeks</h1>
+            <h1 className="management-tip-header">
+              <a href="https://naturesway.com/blogs/articles/8-ways-to-get-through-a-busy-week">
+                Preparing for Busy Weeks
+              </a>              
+            </h1>
             <p className="management-tip-content">Sometimes planning out busy weeks helps reduce overall stress. Here are some tips!</p>
           </div>
 
           <div className="management-tip">
-            <h1 className="management-tip-header">Destressing on a Student Budget!</h1>
+            <h1 className="management-tip-header">
+              <a href="https://money.usnews.com/money/blogs/my-money/articles/affordable-ways-to-relieve-stress-and-unwind">
+                Destressing on a Student Budget!
+              </a>              
+            </h1>
             <p className="management-tip-content">Want to destress after a hectic week, but not sure if you can afford to? Here are some budget friendly options!</p>
           </div>
 
