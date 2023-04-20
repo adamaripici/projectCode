1f5c8d51ff88a6fd027acbcc560309df376ec393
Charlotte Andry <charlotteandry123@gmail.com>
2023-04-20 10:50:05 -0500

    mental health page

--- react/caraway-app/src/mentalHealth.js
+++ react/caraway-app/src/mentalHealth.js
@@ -1,8 +1,48 @@
 import * as React from "react";
 import "./MentalHealth.css";
+import { Link } from 'react-router-dom';
 
 export default function MentalHealth() {
     return (
+    <div className="mental-health-page">
+      <div classname="top">
+        <div className = "front-image">
+          <img src="mentalHealth-Hand.png" alt = "comforting hand"/>
+        </div>
+      </div>
+
+      <div className = "mental-health-title">
         <h1>Mental Health</h1>
+      </div>
+
+      <div className = "all-stories">
+        <div className = "stories">
+          <div className = "story1">
+            <a href="https://www.mind.org.uk/information-support/types-of-mental-health-problems/stress/causes-of-stress/" target="_blank">
+              <img className = "storyImage" src="mentalHealth-mentalHealth.png" alt="girl stressed" />
+              <h3 className="storyTitle">What is mental health?</h3>
+            </a>
+            <p className="hook">Discusses what mental health is.</p>
+          </div>
+          <div className = "story">
+            <a href="https://www.bestcolleges.com/resources/work-life-school-balance/" target="_blank">
+              <img className = "storyImage" src="mentalHealth-HeadPostIts.png" alt="person balancing on a hill" />
+              <h3 className="storyTitle">Mental wellbeing?</h3>
+            </a>
+            <p className="hook">Discusses what mental wellbeing includes.</p>
+          </div>
+          <div className = "story">
+            <a href="https://www.wework.com/ideas/professional-development/creativity-culture/how-to-prioritize-tasks" target="_blank">
+              <img className = "storyImage" src="blogs-story3Pic.jpg" alt="person driving with post-it" />
+              <h3 className="storyTitle">Why mental health?</h3>
+            </a>
+            <p className="hook">Discusses the importance of mental wellness.</p>
+          </div>
+        </div>
+      </div>
+      <div className = "newsletter">
+
+      </div>
+    </div>
     );
 }
