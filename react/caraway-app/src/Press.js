d5cc6e5b023c5f4cbf2f388e2e4c7e2d17258221
adamaripici <55006272+adamaripici@users.noreply.github.com>
2023-04-21 21:53:27 -0500

    press image

--- react/caraway-app/src/Press.js
+++ react/caraway-app/src/Press.js
@@ -3,6 +3,8 @@ import "./Press.css";
 
 export default function Press() {
   return (
-    <h1 className = "Press">Press</h1>
+    <div className="press-div">
+        <h1 className = "Press">Press</h1>
+    </div>
   );
 }
